04355dd6b32286120a33aaa6b1eb50e60f653019
omani <olehhhm@gmail.com>
2021-03-16 16:45:32 +0200

    fix product color migration

--- db/migrate/20210316140233_create_product_colors.rb
+++ db/migrate/20210316140233_create_product_colors.rb
@@ -1,4 +1,4 @@
-class CreateProductInformations < ActiveRecord::Migration[5.2]
+class CreateProductColors < ActiveRecord::Migration[5.2]
   def change
     create_table :product_colors do |t|
       t.references :product, index: true
