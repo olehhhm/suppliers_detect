5cacafc0d08151b272d4e9a73a21ce9b9f3285e2
omani <olehhhm@gmail.com>
2021-03-18 14:32:41 +0200

    add libs folder to app

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,7 @@ module SuppliersDetect
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    
+    config.autoload_paths << Rails.root.join('app/libs')
   end
 end
