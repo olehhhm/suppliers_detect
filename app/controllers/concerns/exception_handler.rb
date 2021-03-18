f9e8cf2ff7c07a969faa7e0d404ccb601074aadc
omani <olehhhm@gmail.com>
2021-03-18 17:06:27 +0200

    fix controller exception handle order

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -2,6 +2,10 @@ module ExceptionHandler
   extend ActiveSupport::Concern
 
   included do
+    rescue_from StandardError do |e|
+      json_response({ message: "Sorry but happen something bad. Please contact our support." }, :internal_server_error)
+    end
+
     rescue_from ActionController::ParameterMissing do |e|
       json_response({ message: e.message }, :bad_request)
     end
@@ -12,10 +16,6 @@ module ExceptionHandler
 
     rescue_from ActiveRecord::RecordInvalid do |e|
       json_response({ message: e.message }, :unprocessable_entity)
-    end
-
-    rescue_from Exception do |e|
-      json_response({ message: "Sorry but happen something bad. Please contact our support." }, :internal_server_error)
     end
   end
 end
